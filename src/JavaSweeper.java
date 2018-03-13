e1ab6ba96f14e8262413d0e8a7b4cadac5de48b9
avtokot <avtokot84@gmail.com>
2018-03-13 09:49:57 +0200

    refactoring

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -3,24 +3,31 @@ import java.awt.*;
 
 public class JavaSweeper extends JFrame {
 
-    JPanel mJPanel;
+    private JPanel mJPanel;
 
     public static void main(String[] args) {
 
         new JavaSweeper();
     }
 
-    public JavaSweeper() {
+    private JavaSweeper() {
+        initPanel();
+        initFrame();
+    }
 
+    private void initPanel() {
         mJPanel = new JPanel();
         mJPanel.setPreferredSize(new Dimension(500, 300));
         add(mJPanel);
-        pack();
+    }
 
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");
         setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false); // запрет на изменения размеров окна
+        pack();
     }
+
 }
