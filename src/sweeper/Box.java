f316510d02ce16bab396d9f9a19e57abd5a222a7
avtokot <avtokot84@gmail.com>
2018-03-18 13:22:38 +0200

    Create package Sweeper and class enam Box with constant

---
+++ src/sweeper/Box.java
@@ -0,0 +1,23 @@
+package sweeper;
+
+/**
+ * Created by go on 18.03.2018.
+ */
+public enum Box {
+    ZERO,
+    NUM1,
+    NUM2,
+    NUM3,
+    NUM4,
+    NUM5,
+    NUM6,
+    NUM7,
+    NUM8,
+    BOMB,
+
+    OPENED,
+    CLOSED,
+    FLAGED,
+    BOMBED,
+    NOBOMB;
+}
