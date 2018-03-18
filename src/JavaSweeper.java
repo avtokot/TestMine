d2e22000e744fb388ad99ed085d5cb52daeefc34
avtokot <avtokot84@gmail.com>
2018-03-18 12:43:32 +0200

    add constant

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -4,6 +4,10 @@ import java.awt.*;
 public class JavaSweeper extends JFrame {
 
     private JPanel mJPanel;
+    private static final int IMAGE_SIZE = 50;
+    private static final int COLS = 15;
+    private static final int ROW = 1;
+
 
     public static void main(String[] args) {
 
@@ -21,12 +25,12 @@ public class JavaSweeper extends JFrame {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.drawImage(getImage("bomb"), 0, 0, this);
-                g.drawImage(getImage("num1"), 50, 0, this);
-                g.drawImage(getImage("num1"), 50, 50, this);
-                g.drawImage(getImage("num1"), 0, 50, this);
+                g.drawImage(getImage("num1"), IMAGE_SIZE, 0, this);
+               /* g.drawImage(getImage("num1"), IMAGE_SIZE, IMAGE_SIZE, this);
+                g.drawImage(getImage("num1"), 0, IMAGE_SIZE, this);*/
             }
         };
-        mJPanel.setPreferredSize(new Dimension(500, 300));
+        mJPanel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROW * IMAGE_SIZE));
         add(mJPanel);
     }
 
